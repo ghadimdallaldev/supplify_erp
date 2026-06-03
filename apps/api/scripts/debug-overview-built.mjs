2bf2013647f0358eb9a1b7a3835e0842ab84c75a
Ghadi Mdallal <ghadimdallaldev@outlook.com>
2026-06-03 13:40:57 +0300

    chore(release): prune preprod tree for EC2 deploy

--- apps/api/scripts/debug-overview-built.mjs
+++
@@ -1,5 +0,0 @@
-import { buildAdminOverviewMetrics } from '../src/lib/admin-overview-metrics.js'
-
-const data = await buildAdminOverviewMetrics()
-console.log(JSON.stringify(data, null, 2))
-process.exit(0)
